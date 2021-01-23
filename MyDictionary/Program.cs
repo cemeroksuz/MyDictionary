c25271cfb9bf73e7923e57d3df1ea54712d89eb1
cem eröksüz <cemeroksuz@outlook.com>
2021-01-23 17:56:13 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cities = new MyDictionary<int, string>();
+
+            cities.Add(1, "Adana");
+            cities.Add(2, "Adıyaman");
+            cities.Add(3, "Afyon");
+            
+
+            for (int i = 0; i < cities.keys.Length; i++)
+            {
+                Console.WriteLine(cities.keys[i] + "->" + cities.values[i]);
+            }
+        }
+    }
+}
